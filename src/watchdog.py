09488ef3fbd9aa4c6120a290fbfd64f73e2d1e93
Maximilian Braunschmied <maxbraunschmied@posteo.at>
2023-07-28 22:31:57 +0200

    added some logs

--- src/watchdog.py
+++ src/watchdog.py
@@ -294,6 +294,7 @@ class Watchdog():
         """ In case mcc isn't a standard value."""
         expected_mccs = [int(e) for e in self.config['general']['expected_mccs'].split(',')]
         if tower.mcc not in expected_mccs:
+            self.logger.warning(f"tower {tower}: mcc is not a standard value!")
             tower.suspiciousness += 30
 
     def check_mnc(self, tower):
@@ -312,6 +313,7 @@ class Watchdog():
         # TODO: the above are all known MNCs in the USA from cell finder's db, but do we really
         # want to include all of them?
         if tower.mnc not in expected_mncs:
+            self.logger.warning(f"tower {tower}: mnc is not a standard value!")
             tower.suspiciousness += 20
 
     def check_existing_rssi(self, tower):
@@ -333,6 +335,7 @@ class Watchdog():
 
             # TODO: think about this some more.
             if tower.rssi is None or tower.rssi > mean + std:
+                self.logger.warning(f"tower {tower}: signal strength after previous record now much higher!")
                 tower.suspiciousness += (tower.rssi - mean)
 
     def check_changed_tac(self, tower):
@@ -347,6 +350,7 @@ class Watchdog():
 
         if existing_tower is not None:
             if existing_tower.tac != tower.tac:
+                self.logger.warning(f"tower {tower}: different tac detected!")
                 tower.suspiciousness += 10
 
     def get_centroid(self, towers):
@@ -395,7 +399,7 @@ class Watchdog():
 
         if int(distance * 10000) > int(radius * 10000):
             s_coeff = (10 * distance - radius) ** 2
-            self.logger.info('tower outside expected range')
+            self.logger.warning('tower outside expected range')
             self.logger.info(f'increasing suspiciousness by {s_coeff}')
 
             tower.suspiciousness += s_coeff
@@ -417,6 +421,7 @@ class Watchdog():
             rssi_std = numpy.std(rssis)
 
             if tower.rssi > rssi_mean + rssi_std:
+                self.logger.warning(f"tower {tower}: high signal strength!")
                 tower.suspiciousness += tower.rssi - rssi_mean
 
     def check_wigle(self, tower):
